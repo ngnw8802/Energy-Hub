f23752127ab94607a2f6d4bcab79fc31f337ad78
JackBlackburnBuffs <113055428+JackBlackburnBuffs@users.noreply.github.com>
2022-11-30 18:12:53 -0700

    Update WattTimeAPIDataFetch.py
    now you can use a command line argument to specify which balancing authority data you want

--- WattTimeAPIDataFetch.py
+++ WattTimeAPIDataFetch.py
@@ -1,6 +1,12 @@
 import requests
+import sys
 from os import path
 from requests.auth import HTTPBasicAuth
+
+#CL INPUT CHECK
+if len(sys.argv) == 1:
+    print("\n\nproper use to get historical data from any BA is: python3 WattTimeAPIDataFetch.py [balancing authority you want]\n\n")
+    quit() 
 
 #THIS LOGS YOU IN AND GIVES YOU YOUR TOKEN
 login_url = 'https://api2.watttime.org/v2/login'
@@ -10,8 +16,9 @@ token = requests.get(login_url, auth=HTTPBasicAuth('justin_energyhub', '4OXv0yc9
 #THIS REQUESTS HISTORICAL DATA AND STORES IT IN LOCAL DIRECTORY IN ZIP FORM
 historical_url = 'https://api2.watttime.org/v2/historical'
 headers = {'Authorization': 'Bearer {}'.format(token)}
-ba = 'CAISO_NORTH'
+ba = sys.argv[1]
 params = {'ba': ba}
+
 #THIS IS THE REQUEST
 rsp = requests.get(historical_url, headers=headers, params=params)
 cur_dir = path.dirname(path.realpath('__file__'))
