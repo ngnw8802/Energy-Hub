227c868f9026dbba198aff40e750ec85c5261e8c
cal-brynestad <cabr8942@colorado.edu>
2023-02-16 11:17:25 -0700

    add data and different index file indexERCO.py
    new singularity data added that has data for all power grids for 2020 and 2021. indexERCO.py was my data analysis file and can be manually merged with index.py at some point depending on what we want in final product

---
+++ components/singularity_erco_2020.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pylab as plt
+
+SingERCOData20 = pd.read_csv('data_2020/ERCO.csv')
---
+++ components/singularity_erco_2021.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pylab as plt
+
+SingERCOData21 = pd.read_csv('data_2021/ERCO.csv')
