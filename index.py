a90d14a42f3b449ea4072d4a080fc49f725b176b
zroy33 <zoeroy33@gmail.com>
2023-03-20 14:12:27 -0600

    calculations.

--- index.py
+++ index.py
@@ -54,6 +54,7 @@ WT_end = end.replace(" ", "T")
 mask = (WattTimeDf['timestamp'] >= WT_start) & (WattTimeDf['timestamp'] <= WT_end)
 WattTimeDf = WattTimeDf.loc[mask]
 
+
 # Display Singularity area chart
 st.write("Singularity Data Area Chart")
 st.area_chart(data=selectBoxChoice, x="datetime_utc", y="consumed_co2_rate_lb_per_mwh_for_electricity")
@@ -62,20 +63,30 @@ st.area_chart(data=selectBoxChoice, x="datetime_utc", y="consumed_co2_rate_lb_pe
 st.write("WattTime Data Area Chart")
 st.area_chart(data=WattTimeDf, x="timestamp", y="MOER")
 
+
 # Display Singularity line chart
-st.write("Singularity Data Line Graph")
-st.line_chart(data= selectBoxChoice,x="datetime_utc",y="consumed_co2_rate_lb_per_mwh_for_electricity")
+#st.write("Singularity Data Line Graph")
+#st.line_chart(data= selectBoxChoice,x="datetime_utc",y="consumed_co2_rate_lb_per_mwh_for_electricity")
 
 # Display WattTime line chart
-st.write("WattTime Data Line Graph")
-st.line_chart(data= WattTimeDf,x="timestamp",y="MOER")
+#st.write("WattTime Data Line Graph")
+#st.line_chart(data= WattTimeDf,x="timestamp",y="MOER")
+
+
+optionBattery = st.selectbox(
+    'How much do you need to charge?',
+    ('10', '20', '30', '40', '60'))
+
+optionChargeType = st.selectbox(
+    'What type of charger do you have?',
+    ('6', '50', '150'))
 
 #caclulations for singularity data
-charge = 30 #kWh
-chargeType = 6 #Level 2 kW
-timeCharge = charge/chargeType
+charge = optionBattery # kWh
+chargeType = optionChargeType # kW
+timeCharge = int(charge)/int(chargeType)
 
-st.write("Calculations for Electric Vehicle")
+#st.write("Calculations for Electric Vehicle")
 start = "2020-02-01 00:00:00-08:00"
 end = "2020-02-01 23:00:00-08:00"
 
@@ -103,11 +114,22 @@ for data in range(len(chunks)-1):
 
 bestMean = min(meanList)
 bestIdx = meanList.index(bestMean)
+chunkIdx = 0
 
 for i in range(len(meanList)):
     if bestIdx==i:
         st.write("Best Time to Charge is:", chunks[i], "to", chunks[i+1])
+        chunkIdx = i
     else:
         continue
 
-st.line_chart(data= day,x="datetime_local",y="consumed_co2_rate_lb_per_mwh_for_electricity")
+fig, axs = plt.subplots()
+day.plot(x="datetime_local",y="consumed_co2_rate_lb_per_mwh_for_electricity", ax=axs)
+#axs.get_xticks(rotation='vertical' )
+axs.axvline(chunks[chunkIdx], color = 'r', label = 'start charge')
+axs.axvline(chunks[chunkIdx+1], color = 'r', label = 'end charge')
+
+st.title('Charging Time Plot')
+st.pyplot(fig)
+
+#st.line_chart(data= day,x="datetime_local",y="consumed_co2_rate_lb_per_mwh_for_electricity")
